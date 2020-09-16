f9c33587f43c313e83f17c58668a0a1f11c2a450
YiWuS <2375061390@qq.com>
2020-09-16 23:55:19 +0800

    Solution853

--- Main.java
+++ Main.java
@@ -1,14 +1,19 @@
 package com.company;
 
-import com.company.排序.Solution1491;
+import com.company.Sort.Solution853;
 
 public class Main {
 
     public static void main(String[] args) {
-        Solution1491 solution=new Solution1491();
-        int[] salary={5,4,2,6,10};
-        solution.average2(salary);
-
+        int target=10;
+                //12;
+        int[] position = {6,8};
+                //{10,8,0,5,3};
+        int[] speed = {3,2};
+                //{2,4,1,1,3};
+        Solution853 solution=new Solution853();
+        int num=solution.carFleet(target,position,speed);
+        System.out.println(num);
 
     }
 }
---
+++ Sort/Solution1528.java
@@ -0,0 +1,38 @@
+package com.company.Sort;
+
+public class Solution1528 {
+    /**
+     * 自己完成：使用辅助字符数组存储后转为字符串返回
+     * @param s
+     * @param indices
+     * @return
+     */
+    public String restoreString(String s, int[] indices) {
+        char[] res=new char[indices.length];
+        int count=0;
+        for (int i=0;i<indices.length;i++){
+            for(int j=0;j<indices.length;j++){
+                if(indices[j]==count){
+                    res[count]=s.charAt(j);
+                    count++;
+                }
+            }
+        }
+        return new String(res);
+    }
+
+    /**
+     * 辅助数组：这里只使用了一次扫描，而我用了两重循环
+     * @param s
+     * @param indices
+     * @return
+     */
+    public String restoreString2(String s, int[] indices) {
+        int length=indices.length;
+        char[] res=new char[length];
+        for(int i=0;i<length;i++){
+            res[indices[i]]=s.charAt(i);
+        }
+        return new String(res);
+    }
+}
