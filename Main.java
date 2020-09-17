b311a5f5cd535a30994fdea9d98c67d5ba763b10
YiWuS <2375061390@qq.com>
2020-09-17 19:47:02 +0800

    Solution56

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import com.company.Sort.Solution56;
 import com.company.Sort.Solution853;
 
 public class Main {
@@ -11,9 +12,14 @@ public class Main {
                 //{10,8,0,5,3};
         int[] speed = {3,2};
                 //{2,4,1,1,3};
-        Solution853 solution=new Solution853();
-        int num=solution.carFleet(target,position,speed);
-        System.out.println(num);
+        int[][] intervals={{1,4},{4,5}};
+        Solution56 solution=new Solution56();
+        int[][] num=solution.merge(intervals);
+        for(int i=0;i<num.length;i++){
+                System.out.print(num[i][0]+" "+num[i][1]);
+                System.out.println();
+
+        }
 
     }
 }
