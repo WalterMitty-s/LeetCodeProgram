027b43a671645f9f8412832f3cd2bf74eb475f86
YiWuS <2375061390@qq.com>
2020-09-18 17:15:20 +0800

    Solution1561

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import com.company.Sort.Solution1561;
 import com.company.Sort.Solution56;
 import com.company.Sort.Solution853;
 
@@ -12,14 +13,11 @@ public class Main {
                 //{10,8,0,5,3};
         int[] speed = {3,2};
                 //{2,4,1,1,3};
+        int[] s1561={2,4,5};
         int[][] intervals={{1,4},{4,5}};
-        Solution56 solution=new Solution56();
-        int[][] num=solution.merge(intervals);
-        for(int i=0;i<num.length;i++){
-                System.out.print(num[i][0]+" "+num[i][1]);
-                System.out.println();
-
-        }
+        Solution1561 solution=new Solution1561();
+//        int[][] num=solution.merge(intervals);
+        System.out.println(solution.maxCoins(s1561));
 
     }
 }
