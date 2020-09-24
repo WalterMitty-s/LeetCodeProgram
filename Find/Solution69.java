8659410154fe4540962ebb56b594b9f0e9203e2c
YiWuS <2375061390@qq.com>
2020-09-24 10:17:20 +0800

    Solution69

--- Find/Solution69.java
+++ Find/Solution69.java
@@ -24,19 +24,21 @@ public class Solution69 {
 
     /**
      * 官方解法：
-     * 二分查找：未看
+     * 二分查找：
+     *  时间复杂度：O(logx) 空间复杂度：O(1)
      * @param x
      * @return
      */
     public int mySqrt2(int x) {
         int l = 0, r = x, ans = -1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if ((long) mid * mid <= x) {
-                ans = mid;
-                l = mid + 1;
-            } else {
-                r = mid - 1;
+        while (r>=l){
+            int mid=l+(r-l)/2;
+            if((long)mid*mid<=x){
+                ans=mid;
+                l=mid+1;
+            }
+            else {
+                r=mid-1;
             }
         }
         return ans;
