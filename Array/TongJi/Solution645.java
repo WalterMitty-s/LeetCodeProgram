b0b6b9d63f345ee1ce86e251358d48198d9e034f
YiWuS <2375061390@qq.com>
2020-10-09 21:52:13 +0800

    Solution645

---
+++ Array/TongJi/Solution645.java
@@ -0,0 +1,26 @@
+package com.company.Array.TongJi;
+
+import java.util.Arrays;
+
+public class Solution645 {
+    /**
+     * 官方解法：排序
+     * 自己想法：
+     *      想到排序，找出重复的，但是无法找出{2,3,3,4,5,6}缺失的1
+     *      但是在官方解法中直接将missing初始化为1
+     * @param nums
+     * @return
+     */
+    public int[] findErrorNums(int[] nums) {
+        Arrays.sort(nums);
+        int dup = -1, missing = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1])
+                dup = nums[i];
+            else if (nums[i] > nums[i - 1] + 1)
+                missing = nums[i - 1] + 1;
+        }
+        return new int[] {dup, nums[nums.length - 1] != nums.length ? nums.length : missing};
+    }
+
+}
--- Main.java
+++ Main.java
@@ -2,14 +2,15 @@ package com.company;
 
 import com.company.Array.BianLi.Solution485;
 import com.company.Array.BianLi.Solution495;
+import com.company.Array.TongJi.Solution645;
 import com.company.Find.Solution50;
 
 public class Main {
     public static void main(String[] args) {
-        Solution495 solution=new Solution495();
-        int[] nums=new int[]{1,2};
-        int num=solution.findPoisonedDuration(nums,2);
-        System.out.println(num);
+        Solution645 solution=new Solution645();
+        int[] nums=new int[]{2,3,3,4,5,6};
+        int[] num=solution.findErrorNums(nums);
+        System.out.println(num[0]+" "+num[1]);
 
     }
 }
