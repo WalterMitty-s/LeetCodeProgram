d07ff778eb8258d882218c5b5d4f673a185bf703
YiWuS <2375061390@qq.com>
2020-10-12 21:48:19 +0800

    Solution442

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 import com.company.Array.BianLi.Solution485;
 import com.company.Array.BianLi.Solution495;
+import com.company.Array.TongJi.Solution442;
 import com.company.Array.TongJi.Solution448;
 import com.company.Array.TongJi.Solution645;
 import com.company.Find.Solution50;
@@ -10,9 +11,9 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Solution448 solution=new Solution448();
+        Solution442 solution=new Solution442();
         int[] nums=new int[]{4,3,2,7,8,2,3,1};
-        List<Integer> list=solution.findDisappearedNumbers(nums);
+        List<Integer> list=solution.findDuplicates(nums);
         for(int i=0;i<list.size();i++){
             System.out.println(list.get(i));
         }
