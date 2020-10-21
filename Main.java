f045952def07869413507d990206b59bc741e437
YiWuS <2375061390@qq.com>
2020-10-21 21:59:23 +0800

    Solution598

--- Main.java
+++ Main.java
@@ -5,17 +5,19 @@ import com.company.Array.ChangeAndMove.Solution283;
 import com.company.Array.ChangeAndMove.Solution665;
 import com.company.Array.TongJi.Solution274;
 import com.company.Array.TwoArray.Solution119;
+import com.company.Array.TwoArray.Solution598;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Solution119 solution=new Solution119();
-        List<Integer> list=solution.getRow(3);
-        for(Integer num:list){
-            System.out.print(num+" ");
+        Solution598 solution=new Solution598();
+        int[][] nums={{2,2},{3,3}};
+        for(int[] num:nums){
+            System.out.println(num[0]+" "+num[1]);
         }
     }
+
 
 }
 
