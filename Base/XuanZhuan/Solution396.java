ca32c670fad63516791ca1ce70d019590dc1970a
YiWuS <2375061390@qq.com>
2020-10-24 16:41:10 +0800

    Solution396

---
+++ Base/XuanZhuan/Solution396.java
@@ -0,0 +1,40 @@
+package com.company.Base.XuanZhuan;
+
+public class Solution396 {
+    /**
+     * 未做出，未看题解
+     * 超出时间限制：
+     * @param A
+     * @return
+     */
+    public int maxRotateFunction(int[] A) {
+        if(A.length==0)
+            return 0;
+        int max=Integer.MIN_VALUE;
+        for(int k=0;k<A.length;k++){
+            int count=0;
+            int result=0;
+            int numOfMove=1;
+            for(int start=0;count<A.length;start++){
+                if(k==0)
+                    break;
+                int prev=A[start];
+                int current = start;
+                do {
+                    int next=(current+numOfMove)%A.length;
+                    int temp=A[next];
+                    A[next]=prev;
+                    prev=temp;
+                    current=next;
+                    count++;
+                } while (start != current);
+            }
+
+            for(int i=0;i<A.length;i++){
+                result=result+i*A[i];
+            }
+           max=Math.max(result,max);
+        }
+        return max;
+    }
+}
--- Main.java
+++ Main.java
@@ -3,12 +3,13 @@ package com.company;
 
 import com.company.Base.Array.TwoArray.Solution598;
 import com.company.Base.XuanZhuan.Solution189;
+import com.company.Base.XuanZhuan.Solution396;
 
 public class Main {
     public static void main(String[] args) {
-        Solution189 s=new Solution189();
-        int[] nums = {1, 2, 3, 4};
-        s.rotate(nums,2);
+        Solution396 s=new Solution396();
+        int[] nums = {4,3,2,6};
+        s.maxRotateFunction(nums);
     }
 
 
